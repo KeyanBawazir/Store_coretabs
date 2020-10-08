cfd1939b5ce5ae010276c8e7aca3c1d44bcbebb8
KeyanBawazir <key42pc@gmail.com>
2020-10-08 19:05:09 +0300

    cart and order apps

---
+++ orders/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from products.models import Product
+
+User = get_user_model()
+# Create your models here.
+class Order(models.Model):
+    address = models.CharField(max_length=150, help_text='Enter Your Address')
+    user = models.ForeignKey(User, related_name='order', on_delete=models.CASCADE)
+    items = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return str(self.user)
